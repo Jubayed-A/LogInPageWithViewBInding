4741e2ceddbb532322805d3503d80982772b0198
Jubayed-A <learnigprograming@gmail.com>
2023-11-17 11:07:31 +0600

    New project With ui Design

---
+++ app/src/main/java/com/example/loginfromwithtc/SigInActivity.kt
@@ -0,0 +1,11 @@
+package com.example.loginfromwithtc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SigInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sig_in)
+    }
+}
